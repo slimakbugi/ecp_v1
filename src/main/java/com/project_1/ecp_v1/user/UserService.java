a42f74a0a45125b8bdbc4d6fda10557226a5b87c
Kamil <kamil@mail.com>
2025-03-10 21:43:33 +0100

    20250310 EOD

--- src/main/java/com/project_1/ecp_v1/user/UserService.java
+++ src/main/java/com/project_1/ecp_v1/user/UserService.java
@@ -3,7 +3,10 @@ package com.project_1.ecp_v1.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.StreamSupport;
 
 @Service
 public class UserService {
@@ -22,6 +25,12 @@ public class UserService {
                 .map(userMapper::userToDtoMapper);
     }
 
+    public List<UserDTO> getAllUsers(){
+        return StreamSupport.stream(userRepository.findAll().spliterator(), false)
+                .map(userMapper::userToDtoMapper)
+                .toList();
+    }
+
     public boolean deleteUser(Integer id){
         if(!userRepository.existsById(id)){
             return false;
@@ -36,5 +45,51 @@ public class UserService {
 
         userRepository.save(user);
         return userMapper.userToDtoMapper(user);
+    }
+
+    public Optional<UserDTO> partiallyUpdateUser(Integer id, UserCreationDTO userCreationDTO){
+        return userRepository.findById(id)
+                .map(existingUser -> {
+                    if (userCreationDTO.firstname() != null) existingUser.setFirstname(userCreationDTO.firstname());
+                    if (userCreationDTO.lastname() != null) existingUser.setLastname(userCreationDTO.lastname());
+                    if (userCreationDTO.email() != null) existingUser.setEmail(userCreationDTO.email());
+                    if (userCreationDTO.dateOfBirth() != null) existingUser.setDateOfBirth(userCreationDTO.dateOfBirth());
+                    if (userCreationDTO.position() != null) existingUser.setPosition(userCreationDTO.position());
+                    if (userCreationDTO.employmentDate() != null) existingUser.setEmploymentDate(userCreationDTO.employmentDate());
+
+                    return userRepository.save(existingUser);
+                })
+                .map(userMapper::userToDtoMapper);
+    }
+
+    public boolean releaseUser(Integer id, LocalDate releaseDate){
+        if (userRepository.existsById(id)) {
+            userRepository.findById(id)
+                    .map(user -> {
+                        user.setIsHired(false);
+                        user.setReleaseDate(releaseDate);
+
+                        return userRepository.save(user);
+                    });
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean hireUserAgain(Integer id, LocalDate employmentDate){
+        if (userRepository.existsById(id)) {
+            userRepository.findById(id)
+                    .map(user -> {
+                        user.setIsHired(true);
+                        user.setReleaseDate(LocalDate.of(2099,12,31));
+                        user.setEmploymentDate(employmentDate);
+
+                        return userRepository.save(user);
+                    });
+            return true;
+        } else {
+            return false;
+        }
     }
 }
