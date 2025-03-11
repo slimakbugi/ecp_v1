e47126814d37685d26538aca71cd3970aded65b2
Kamil <kamil@mail.com>
2025-03-11 23:09:59 +0100

    20250311 EOD

--- src/main/java/com/project_1/ecp_v1/user/UserController.java
+++ src/main/java/com/project_1/ecp_v1/user/UserController.java
@@ -1,9 +1,13 @@
 package com.project_1.ecp_v1.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
@@ -14,6 +18,46 @@ public class UserController {
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<UserDTO>> getAllUsers(){
+        List<UserDTO> allUsers = userService.getAllUsers();
+
+        return ResponseEntity.ok(allUsers);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> getUser(@PathVariable Integer id){
+        Optional<UserDTO> user = userService.getUserDtoById(id);
+
+        return user
+                .map(u -> ResponseEntity.ok().body(u))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping()
+    public ResponseEntity<UserDTO> addUser(@RequestBody UserCreationDTO user){
+        UserDTO addedUser = userService.addUser(user);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(addedUser.id())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(addedUser);
+    }
+
+    @DeleteMapping()
+    public ResponseEntity<Void> deleteUser(@PathVariable Integer id){
+        boolean isUserDeleted = userService.deleteUser(id);
+
+        if(isUserDeleted){
+            return ResponseEntity.accepted().build();
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
+
     }
 
 }
